939e49c0b2f889fde87e33c9b532780718596dfc
bqardi <51706945+bqardi@users.noreply.github.com>
2020-09-29 20:35:06 +0200

    Rearranged files + updated path to JSON file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 import getData from "./modules/fetch.js";
 import cards from "./modules/card.js";
 
-getData("./assets/data/database.json").then(onLoadData);
+getData("./api/index.json").then(onLoadData);
 
 function onLoadData(data){
     let board = document.querySelector(".board");
