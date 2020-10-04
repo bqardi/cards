1c796c03f2feaa2a94515c7ba42fa750994f7ff0
bqardi <51706945+bqardi@users.noreply.github.com>
2020-10-04 15:43:33 +0200

    Small example added
    Created a small draw-cards-example.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,62 @@
-import getData from "./modules/fetch.js";
-import cards from "../../api/card.js";
+import getCards from "../../api/card.js";
 
-getData("./api/index.json").then(onLoadData);
+getCards(function(cards){
 
-function onLoadData(data){
+    // Create the drawnCards container
+    let drawnCards = createDrawnCardsContainer();
+
+    // Create the deck
+    let deck = createDeck(cards, drawnCards);
+
+    // Shuffle the cards
+    cards.shuffleDeck();
+
+    // Append the deck and the drawnCards (container) element
     let board = document.querySelector(".board");
-    let card = cards.createCard(data.cards[12].image);
-    board.appendChild(card);
+    board.appendChild(deck);
+    board.appendChild(drawnCards);
+});
+
+function createDeck(cardsObj, container){
+    // Create a card with backface up (simulating the deck)
+    let deck = createCard(cardsObj.back.blue.image);
+
+    // Set an event listener to listen for clicks on the deck
+    deck.addEventListener("click", function(){
+        // Stop displaying the deck when there is no more cards
+        if (cardsObj.deck.length === 0) {
+            deck.classList.add("js-empty");
+            return;
+        }
+
+        // Append each drawn card to the drawnCards element
+        let drawn = createCard(cardsObj.draw().image);
+        container.appendChild(drawn);
+    });
+
+    return deck;
+}
+
+function createDrawnCardsContainer(){
+    // Just create an empty container and return it
+    let drawnCards = document.createElement("DIV");
+    drawnCards.classList.add("card__container");
+    return drawnCards;
+}
+
+function createCard(imgSrc){
+    // Create the card element
+    let card = document.createElement("DIV");
+    card.classList.add("card");
+    
+    // Create the img and set src to whatever is
+    // passed in as an argument (imgSrc)
+    let img = document.createElement("IMG");
+    img.src = imgSrc;
+    img.classList.add("card__image");
+
+    // Append the img to the card
+    card.appendChild(img);
+
+    return card;
 }
